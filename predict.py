7adcef6276afb6fb59446fa7f70ad99b6f5d9422
Winshan <839478021@qq.com>
2021-12-27 17:05:55 +0800

    Signed-off-by: Winshan <839478021@qq.com>

---
+++ predict.py
@@ -0,0 +1,49 @@
+import time
+import cv2
+import numpy as np
+from PIL import Image
+
+from unet import Unet
+
+if __name__ == "__main__":
+    #-------------------------------------------------------------------------#
+    #   如果想要修改对应种类的颜色，到__init__函数里修改self.colors即可
+    #-------------------------------------------------------------------------#
+    unet = Unet()
+    #----------------------------------------------------------------------------------------------------------#
+    #   mode用于指定测试的模式：
+    #   'dir_predict'表示遍历文件夹进行检测并保存。默认遍历img文件夹，保存img_out文件夹，详情查看下方注释。
+    #----------------------------------------------------------------------------------------------------------#
+    mode = "predict"
+    dir_origin_path = "img/"
+    dir_save_path   = "img_out/"
+
+    if mode == "predict":
+
+        while True:
+            img = input('Input image filename:')
+            try:
+                image = Image.open(img)
+            except:
+                print('Open Error! Try again!')
+                continue
+            else:
+                r_image = unet.detect_image(image)
+                r_image.show()
+        
+    elif mode == "dir_predict":
+        import os
+        from tqdm import tqdm
+
+        img_names = os.listdir(dir_origin_path)
+        for img_name in tqdm(img_names):
+            if img_name.lower().endswith(('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
+                image_path  = os.path.join(dir_origin_path, img_name)
+                image       = Image.open(image_path)
+                r_image     = unet.detect_image(image)
+                if not os.path.exists(dir_save_path):
+                    os.makedirs(dir_save_path)
+                r_image.save(os.path.join(dir_save_path, img_name))
+                
+    else:
+        raise AssertionError("Please specify the correct mode: 'predict',  or 'dir_predict'.")
